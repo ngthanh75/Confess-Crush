9982796224620a76ef153342578d08a269ac0393
ngthanh75 <117805306+ngthanh75@users.noreply.github.com>
2022-11-09 14:06:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 const CONFIG = {
-    titleWeb: "gửi bé iu",
+    titleWeb: "gửi em bé Xu",
     introTitle: 'Trần Thị Thùy Dương',
     introDesc: `gửi công chúa của anh,
-    nhưng anh sẽ cố gắng dành thời gian cho em nhiều nhất có thể
-    đợi anh nhá`,
+    có thể thời gian này anh hơi bận một chút 
+    nhưng anh sẽ cố gắng dành thời gian cho em 
+    hãy tin ở anh giáng sinh này mình sẽ bên nhau nha`,
     btnIntro: 'anh yêu em',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    btnYes: 'dạ em yêu ',
     btnNo: 'Không nha :3',
-    question: 'tại sao em lại chọn yêu anh',
+    question: 'tại sao em lại chọn anh',
     btnReply: 'Gửi cho bạn <3',
     reply: 'đợi anh nhé bbi',
     mess: 'Anh biết mà 🥰. Yêu bé nhiều lắm 😘😘',
     messDesc: 'đợi anh nha, giáng sinh anh sẽ ở bên em ',
-    btnAccept: 'Okiiiii lun <3',
+    btnAccept: 'dạ anh yêu em',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
